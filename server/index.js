831cfce735b1ceffe0d62c81de77bbc8f4ee7067
JayChuang8 <jaychuang1@gmail.com>
2023-02-13 01:17:50 -0700

    Change server configuration

--- server/index.js
+++ server/index.js
@@ -5,15 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+const PORT = process.env.PORT || 9000;
+
+var testAPI = require("./api/testAPI");
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -22,14 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use("/testAPI", testAPI);
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -43,3 +32,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
