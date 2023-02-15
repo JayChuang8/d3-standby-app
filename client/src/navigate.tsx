f5803dcabe574a92db8d722a2cd6ed9665e67be5
Madisson Carle <63175775+MadissonCarle@users.noreply.github.com>
2023-02-15 13:45:11 -0700

    navigate

--- client/src/navigate.tsx
+++ client/src/navigate.tsx
@@ -1,54 +1,86 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import Carousel from "react-material-ui-carousel";
-import Typography from "@material-ui/core/Typography";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { observer } from "mobx-react-lite";
+import axios from "axios";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
-function navigate() {
-  // State to programmatically set active child
-  const [activeChild, setActiveChild] = useState(0);
+export const App = observer(() => {
+  const [testData, setTestData] = useState([]);
+  const [sshData, setSshData] = useState([]);
+  const [ipAddress, setIpAddress] = useState("");
+  const moveTo = async (x: number, y: number) => {
+    const tx = await systems["system.Move"].executeTyped({ x, y });
+    await tx.wait();
+  };
 
-  // Basically items = [1, 2, 3, 4]
-  const items = useMemo(() => [1, 2, 3, 4], []);
+  const moveBy = async (deltaX: number, deltaY: number) => {
 
-  // The Keypress Event Handler
-  const changeChild = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === "ArrowLeft") {
-        // If supposed previous child is < 0 set it to last child
-        setActiveChild((a) => (a - 1 < 0 ? items.length - 1 : a - 1));
-      } else if (e.key === "ArrowRight") {
-        // If supposed next child is > length -1 set it to first child
-        setActiveChild((a) => (a + 1 > items.length - 1 ? 0 : a + 1));
-      }
-    },
-    [items]
-  );
+  };
 
-  // Set and cleanup the event listener
-  useEffect(() => {
-    document.addEventListener("keydown", changeChild);
-
-    return function cleanup() {
-      document.removeEventListener("keydown", changeChild);
-    };
-  });
-
+  const listener = (e: KeyboardEvent) => {
+    if (e.key === "ArrowUp") {
+      moveBy(0, -1);
+    }
+    if (e.key === "ArrowDown") {
+      moveBy(0, 1);
+    }
+    if (e.key === "ArrowLeft") {
+      moveBy(-1, 0);
+    }
+    if (e.key === "ArrowRight") {
+      moveBy(1, 0);
+    }
+  };
   return (
-    <div className="App">
-      <Carousel
-        index={activeChild} // <-- This controls the activeChild
-        autoPlay={false} // <-- You probaly want to disable this for our purposes
-        navButtonsAlwaysVisible
-      >
-        {items.map((i) => {
-          return (
-            <Typography align="center" key={i}>
-              Child {i}
-            </Typography>
-          );
-        })}
-      </Carousel>
+    <div className="navigate">
+      <header className="App-header">
+        <div>
+          <TextField
+            label="IP Address of d3 Robot"
+            variant="filled"
+            sx={{
+              input: {
+                background: "white",
+              },
+            }}
+            onChange={(o) => setIpAddress(o.target.value)}
+          />
+        </div>
+        <div>
+          <Button
+            disabled={!ipAddress}
+            variant="contained"
+            onClick={async () => {
+              await axios
+                .get("https://d3-standby-server.vercel.app/ssh/stream", {
+                  params: { ip: ipAddress },
+                })
+                .then((response) => setSshData(response.data));
+              console.log("response gotten:", sshData);
+            }}
+          >
+            Connect to d3 through SSH
+          </Button>
+        </div>
+        <p>The ssh response is: {sshData}</p>
+        <div>
+          <Button
+            variant="contained"
+            onClick={async () => {
+              await axios.post(
+                "https://d3-standby-server.vercel.app/ssh/download",
+                {
+                  params: { ip: ipAddress },
+                }
+              );
+            }}
+          >
+            Download file using SCP
+          </Button>
+        </div>
+      </header>
     </div>
   );
-}
-
-export default navigate;
+});
