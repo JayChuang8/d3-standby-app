817258ced8c87bb429cfb4a10f2ed8529647ae99
JayChuang8 <jaychuang1@gmail.com>
2023-02-15 00:51:17 -0700

    Added buttons to download data and start stream

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,79 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { observer } from 'mobx-react-lite';
-import axios from 'axios';
-import Button from '@mui/material/Button';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { observer } from "mobx-react-lite";
+import axios from "axios";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 export const App = observer(() => {
   const [testData, setTestData] = useState([]);
   const [sshData, setSshData] = useState([]);
+  const [ipAddress, setIpAddress] = useState("");
 
-  useEffect(() => {
-    axios.get("https://d3-standby-server.vercel.app/testAPI")
-      .then(response => setTestData(response.data))
-  }, [])
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {testData}
-        </p>
-        <Button variant="contained" onClick={() => {
-          axios.get("https://d3-standby-server.vercel.app/sshConn")
-          .then(response => setSshData(response.data))
-        }}>Connect to d3 through SSH</Button>
-        <p>
-          The ssh response is: {sshData}
-        </p>
+        <Button
+          variant="contained"
+          onClick={async () => {
+            await axios
+              .get("https://d3-standby-server.vercel.app/testAPI/test", {
+                params: { id: 1 },
+              })
+              .then((response) => setTestData(response.data));
+            console.log("response gotten:", testData);
+          }}
+        >
+          Test API
+        </Button>
+        <p>{testData}</p>
+        <div>
+          <TextField
+            label="IP Address of d3 Robot"
+            variant="filled"
+            sx={{
+              input: {
+                background: "white",
+              },
+            }}
+            onChange={(o) => setIpAddress(o.target.value)}
+          />
+        </div>
+        <div>
+          <Button
+            disabled={!ipAddress}
+            variant="contained"
+            onClick={async () => {
+              await axios
+                .get("https://d3-standby-server.vercel.app/ssh/stream", {
+                  params: { ip: ipAddress },
+                })
+                .then((response) => setSshData(response.data));
+              console.log("response gotten:", sshData);
+            }}
+          >
+            Connect to d3 through SSH
+          </Button>
+        </div>
+        <p>The ssh response is: {sshData}</p>
+        <div>
+          <Button
+            variant="contained"
+            onClick={async () => {
+              await axios.post(
+                "https://d3-standby-server.vercel.app/ssh/download",
+                {
+                  params: { ip: ipAddress },
+                }
+              );
+            }}
+          >
+            Download file using SCP
+          </Button>
+        </div>
       </header>
     </div>
   );
-})
+});
