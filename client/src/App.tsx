39aeb39641e27f85e2fd20be4a8f7b3762043ff4
rayrayzee <114440061+rayrayzee@users.noreply.github.com>
2023-02-15 17:08:27 -0700

    adding logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+import logo from "./test.svg";
 import "./App.css";
 import { observer } from "mobx-react-lite";
 import axios from "axios";
